4b0c39db6880a85c45d4d97c98355bf8054fcd80
Douglas Duteil <douglasduteil@gmail.com>
2014-05-15 14:23:09 +0200

    refacto: gulp the thing

--- test/karma-jqlite.conf.js
+++ test/karma-jqlite.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'test/helpers/jquery_remove.js',
       'test/helpers/browserTrigger.js',
       'bower_components/angular/angular.js',
