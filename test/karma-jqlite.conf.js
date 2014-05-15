1ee4bb6ac8ff4ff7fdb671f4436a300124e478d1
Douglas Duteil <douglasduteil@gmail.com>
2014-05-15 12:55:30 +0200

    chore(dep): add rAF polyfill bower dependency

--- test/karma-jqlite.conf.js
+++ test/karma-jqlite.conf.js
@@ -22,6 +22,7 @@ module.exports = function (config) {
       'test/helpers/browserTrigger.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/raf/index.js',
       'src/*',
       'test/*.spec.js'
     ],
