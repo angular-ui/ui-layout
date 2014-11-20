ffbb1f49ecd10988de47a34e045e74970a5e7d51
Jonathan Broquist <jonnybro@gmail.com>
2014-11-19 20:40:26 -0700

    chore(~): fix bower main property
    Set the main property in the bower.json file automatically generated by the angular-ui publishing system to the proper path and values.
    
    Resolves #51

--- publish.js
+++ publish.js
@@ -12,6 +12,9 @@ module.exports = function() {
     inlineHTML : fs.readFileSync(__dirname + '/demo/demo.html'),
     inlineJS : 'angular.module(\'doc.ui-layout\', [\'ui.layout\', \'prettifyDirective\', \'ui.bootstrap\', \'plunker\' ]);',
     css : ['demo/demo.css', 'dist/ui-layout.css'],
-    js : ['dist/ui-layout.js']
+    js : ['dist/ui-layout.js'],
+    bowerData: {
+      main: ['ui-layout.js', 'ui-layout.css']
+    }
   };
 };
