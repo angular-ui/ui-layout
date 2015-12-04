220cbb76b145bdc5407ab1f8d14e48e9c194e594
Hannes Widmoser <widmoser@gmail.com>
2015-12-04 14:21:53 +0100

    style(persistentState): fixed jshint problems

--- src/ui-layout.js
+++ src/ui-layout.js
@@ -4,7 +4,8 @@
  * UI.Layout
  */
 angular.module('ui.layout', [])
-  .controller('uiLayoutCtrl', ['$scope', '$attrs', '$element', '$timeout', '$window', 'LayoutContainer', 'Layout', function uiLayoutCtrl($scope, $attrs, $element, $timeout, $window, LayoutContainer, Layout) {
+  .controller('uiLayoutCtrl', ['$scope', '$attrs', '$element', '$timeout', '$window', 'LayoutContainer', 'Layout',
+    function uiLayoutCtrl($scope, $attrs, $element, $timeout, $window, LayoutContainer, Layout) {
 
     var ctrl = this;
     var opts = angular.extend({}, $scope.$eval($attrs.uiLayout), $scope.$eval($attrs.options));
@@ -193,7 +194,7 @@ angular.module('ui.layout', [])
         var c = ctrl.containers[i];
         c.storageId = ctrl.id + ':' + i;
       }
-    };
+    }
 
     //================================================================================
     // Public Controller Functions
